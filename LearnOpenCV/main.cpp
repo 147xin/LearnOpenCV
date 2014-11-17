923b511586c43f6f59509a6f7ee5ec696cf3a746
hix <yef147@gmail.com>
2014-11-17 17:07:01 +0800

    Initial Commit

---
+++ LearnOpenCV/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnOpenCV
+//
+//  Created by Yixin Huang on 14/11/17.
+//  Copyright (c) 2014年 DML. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
