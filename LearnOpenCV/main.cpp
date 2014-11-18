b55468b331a9fe811d526addef42767d648eb0b9
hix <yef147@gmail.com>
2014-11-18 15:37:14 +0800

    initial commit

--- LearnOpenCV/main.cpp
+++ LearnOpenCV/main.cpp
@@ -7,9 +7,31 @@
 //
 
 #include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main(int argc, const char* argv[])
+{
+    cv::Mat image;
+    cout << "size: " << image.size().height << "," << image.size().width << endl;
+    
+    image=cv::imread("/Users/hix/XcodeProjects/LearnOpenCV/LearnOpenCV/test.jpg");
+    if (!image.data) {
+        cout << "图像未创建" << endl;
+    }
+    cv::namedWindow("Original Image");
+    cv::imshow("Original Image", image);
+    cv::waitKey(0);
+    cv::Mat result;
+    cv::flip(image, result, 1);
+    
+    cv::namedWindow("Output Image");
+    cv::imshow("Output Image", result);
+    cv::waitKey(0);
+    
+    cv::imwrite("/Users/hix/XcodeProjects/LearnOpenCV/LearnOpenCV/output.bmp", result);
     return 0;
 }
